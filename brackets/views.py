8df274c626e09473e978c94ff0efcd3d4451bacf
Andrew <68520911+andrewloftus24@users.noreply.github.com>
2021-11-04 17:33:33 -0400

    Creating develop branch, bringing in changes from main.

--- brackets/views.py
+++ brackets/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import spotipy
 
 # Create your views here.
+from django.urls import reverse
+
 from brackets.models import Brackets
 
 
@@ -28,6 +30,69 @@ def login(request):
         'log': log,
     }
     return render(request, 'brackets/login.html', context=context)
+
+
+def top50(request):
+    scope = 'user-top-read'
+    response = HttpResponse()
+
+    auth_manager = spotipy.oauth2.SpotifyClientCredentials(client_id='',
+                                                           client_secret='')
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    playlists = sp.user_playlists('spotify')
+    for x in range(50):
+        response.write(playlists['items'][x]['description'])
+        response.write('</br>')
+
+    return HttpResponse(response)
+
+
+def bracket(request, artist_name):
+    # Setups
+    scope = 'user-top-read'
+    response = HttpResponse()
+    auth_manager = spotipy.oauth2.SpotifyClientCredentials(client_id='f14faa95f781483da8d47a43edc9882b',
+                                                           client_secret='9d732406270743ff97d5acf2300962cd')
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    name = artist_name
+
+    # Get the first artist from results, take their uri
+    results = sp.search(q='artist:' + name, type='artist')
+    uri = results['artists']['items'][0]['uri']
+
+    # Search that uri for the top 8 songs for the bracket, stored as name in tracks
+    results = sp.artist_top_tracks(uri)
+    tracks = []
+    for x in range(10):
+        tracks.append(str(results['tracks'][x]['name']))
+
+    #track_names = Brackets.objects.filter()
+    #context = {
+    #   "name": track_names,
+    #}
+
+    #for x in range(10):
+    #    response.write(str(x + 1) + '. ' + tracks[x])
+    #    response.write('<br/>')
+
+    #return HttpResponse(response)
+    return tracks
+
+def top_artists(request):
+    names = Brackets.objects.filter()
+    context = {
+        'names': names,
+    }
+    return render(request, 'brackets/bracket.html', context=context)
+
+def search_artist(request):
+    artist = request.POST.get('artist', 'none')
+    tracks = bracket(request, artist)
+    for track in tracks:
+        x = Brackets(song_name=track)
+        x.save()
+    return HttpResponseRedirect(reverse('bracket'))
 
 
 def top50(request):
