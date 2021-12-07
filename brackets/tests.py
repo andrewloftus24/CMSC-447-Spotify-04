8225dc9248c29dbb2927ea7735963f02c23b0250
IsaiahWildes <isaiahwildes85@gmail.com>
2021-12-07 16:20:42 -0500

    Added Selenium to the project. For now it runs a simple function to test logging in. In the future logging in needs to be verified and other tests will be added.

--- brackets/tests.py
+++ brackets/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# Create your tests here.
+class LoginTest(LiveServerTestCase):
+    port = 8000
+
+    def testlogin(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get('http://127.0.0.1:8000/')
+        loginButton = driver.find_element_by_link_text('Login')
+
+        loginButton.send_keys(Keys.RETURN)
+        username= driver.find_element_by_id("login-username")
+        password = driver.find_element_by_id("login-password")
+
+        username.send_keys('447spotify04@gmail.com')
+        password.send_keys('Quasar04')
+        submitButton = driver.find_element_by_id("login-button")
+        submitButton.send_keys(Keys.RETURN)
